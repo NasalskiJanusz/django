db17067407b96d20ad815a18db89f3d837b79e0e
Janusz Nasalski <nasalski.janusz@gmail.com>
2024-11-12 04:48:32 +0100

    created simple django app

---
+++ introduction.py
@@ -0,0 +1,8 @@
+# how to start project?
+# go into your directory, open cmd in there and type:
+# django-admin startproject (name)
+
+# how to start local serwer?
+# go into directory with manage.py
+# open cmd in there and type:
+# python manage.py runserver (port if needed)
