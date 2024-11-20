fb414f9dffae2a29da783cf43fa7c4a0c4b62126
Janusz Nasalski <nasalski.janusz@gmail.com>
2024-11-20 08:30:58 +0100

    update, admin panel improvements (new sql tabels and functions to show them in the panel)

--- introduction.py
+++ introduction.py
@@ -1,3 +1,5 @@
+# ---------------------- START ------------------------------
+
 # how to start project?
 # go into your directory, open cmd in there and type:
 # django-admin startproject (name)
@@ -18,9 +20,178 @@
 # each endpoint is endpoint to our aplication
 # we can ahe multiple aplications in one project
 # 
-#
-#
-#
-# 
 # dont forget to create urls.py in our aplication folder
 # 
+
+
+
+
+#--------------------- DATA BASE ---------------------------
+
+# database introduction in django
+# add aplication in settings.py (INSTALLED_APPS)
+# 'name.apps.NameConfig'
+
+# in models.py create classes that will be used to modify our data
+
+# after that make sure to put this command in cmd:
+# python manage.py makemigrations name_of_our_aplication
+# THINK OF IT LIKE OF COMMITING IN GITHUB (APPLAYING CHANGES TO OUR PROJECT)
+# now use command:
+# python manage.py migrate
+
+
+#------------------ ADMIN PANEL -----------------------------
+# how to create superuser
+# python manage.py createsuperuser
+
+# how to add our tables into admin panel?
+# go to admin.py and type:
+# from .models import name1,name2
+# admin.site.register(name1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------ SHELL ------------------------
+# python manage.py shell
+
+
+# in shell you can modify things in database
+
+# make sure to import those items (that you want to modify)
+# from app_name.models import item_name, item_name2, item_name3...
+
+# From here you can make variable that stores the data for ex. :
+# t = ToDoList(name="Tim\'s List")
+
+# To save it just type 
+# variable_name.save()
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
--- youtube/ideas/apps.py
+++ youtube/ideas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class IdeasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'ideas'
